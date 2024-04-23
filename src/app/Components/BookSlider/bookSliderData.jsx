ff765cadec15d00b1d8cf93c15ad50d96549cb2f
Anita <88983297+anita-ebube@users.noreply.github.com>
2024-04-23 13:56:17 +0100

    initial commit

--- src/app/Components/BookSlider/bookSliderData.jsx
+++ src/app/Components/BookSlider/bookSliderData.jsx
@@ -1,70 +1,70 @@
-import CoreCourses from "../BookSlider/BookSlider";
+// import CoreCourses from "../BookSlider/BookSlider";
 
-const coursesData = [
-  {
-    href: "/",
-    image: "/images/notes/5.png",
-    title: "Cos 101: Artificail Intelligence",
-    hint: "Explore the world of database",
-    author: "/images/notes/author.png",
-    name: "Dr M.C Okoronkwo ",
-    COS232:
-      "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
-    download: "/images/sidebar/icons/download.svg",
-  },
-  {
-    href: "/",
-    image: "/images/notes/2.png",
-    title: "Cos 101: Data management",
-    hint: "Explore the world of database",
-    author: "/images/notes/author.png",
-    name: "Dr M.C Okoronkwo ",
-    COS232:
-      "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
-    download: "/images/sidebar/icons/download.svg",
-  },
-  {
-    href: "/",
-    image: "/images/notes/3.png",
-    title: "Cos 101: Data Structures",
-    hint: "Explore the world of database",
-    author: "/images/notes/author.png",
-    name: "Dr M.C Okoronkwo ",
-    COS232:
-      "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
-    download: "/images/sidebar/icons/download.svg",
-  },
-  {
-    href: "/",
-    image: "/images/notes/6.png",
-    title: "Cos 101: Artificail Intelligence",
-    hint: "Explore the world of database",
-    author: "/images/notes/author.png",
-    name: "Dr M.C Okoronkwo ",
-    COS232:
-      "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
-    download: "/images/sidebar/icons/download.svg",
-  },
-  {
-    href: "/",
-    image: "/images/notes/1.png",
-    title: "Cos 101: Artificail Intelligence",
-    hint: "Explore the world of database",
-    author: "/images/notes/author.png",
-    name: "Dr M.C Okoronkwo ",
-    COS232:
-      "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
-    download: "/images/sidebar/icons/download.svg",
-  },
-];
+// const coursesData = [
+//   {
+//     href: "/",
+//     image: "/images/notes/5.png",
+//     title: "Cos 101: Artificail Intelligence",
+//     hint: "Explore the world of database",
+//     author: "/images/notes/author.png",
+//     name: "Dr M.C Okoronkwo ",
+//     COS232:
+//       "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
+//     download: "/images/sidebar/icons/download.svg",
+//   },
+//   {
+//     href: "/",
+//     image: "/images/notes/2.png",
+//     title: "Cos 101: Data management",
+//     hint: "Explore the world of database",
+//     author: "/images/notes/author.png",
+//     name: "Dr M.C Okoronkwo ",
+//     COS232:
+//       "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
+//     download: "/images/sidebar/icons/download.svg",
+//   },
+//   {
+//     href: "/",
+//     image: "/images/notes/3.png",
+//     title: "Cos 101: Data Structures",
+//     hint: "Explore the world of database",
+//     author: "/images/notes/author.png",
+//     name: "Dr M.C Okoronkwo ",
+//     COS232:
+//       "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
+//     download: "/images/sidebar/icons/download.svg",
+//   },
+//   {
+//     href: "/",
+//     image: "/images/notes/6.png",
+//     title: "Cos 101: Artificail Intelligence",
+//     hint: "Explore the world of database",
+//     author: "/images/notes/author.png",
+//     name: "Dr M.C Okoronkwo ",
+//     COS232:
+//       "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
+//     download: "/images/sidebar/icons/download.svg",
+//   },
+//   {
+//     href: "/",
+//     image: "/images/notes/1.png",
+//     title: "Cos 101: Artificail Intelligence",
+//     hint: "Explore the world of database",
+//     author: "/images/notes/author.png",
+//     name: "Dr M.C Okoronkwo ",
+//     COS232:
+//       "https://drive.google.com/uc?export=download&id=1qMZ1kYYW-qQv20JsMdm7LReQ83HPjQVh",
+//     download: "/images/sidebar/icons/download.svg",
+//   },
+// ];
 
-const BookSliderData = () => {
-  return (
-    <div>
-      {coursesData && coursesData.length > 0 && (
-        <CoreCourses courses={coursesData} />
-      )}
-    </div>
-  );
-};
-export default BookSliderData;
+// const BookSliderData = () => {
+//   return (
+//     <div>
+//       {coursesData && coursesData.length > 0 && (
+//         <CoreCourses courses={coursesData} />
+//       )}
+//     </div>
+//   );
+// };
+// export default BookSliderData;

9103410f2a46ad6c63fd4146b06cd69ce822f9b2
Anita <88983297+anita-ebube@users.noreply.github.com>
2024-04-23 12:21:07 +0100

    Still trying to solve a bug

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../Components/Navbar/navbar";
+import Navbar from "../Components/Navbar/Navbar";
 import Sidebar from "../Components/Sidebar/Sidebar";
 import Auth from "../../auth";
 
